50fe4eda0fb47a995a08026e280eed2863ecd0da
itcast <hello@itheima.cn>
2025-09-22 20:30:15 +0800

    中级代码推送

--- src/main/java/com/yupi/yuaicodemother/controller/HealthController.java
+++ src/main/java/com/yupi/yuaicodemother/controller/HealthController.java
@@ -1,14 +1,17 @@
 package com.yupi.yuaicodemother.controller;
 
+import com.yupi.yuaicodemother.common.BaseResponse;
+import com.yupi.yuaicodemother.common.ResultUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/health")
+public class HealthController {
+
     @GetMapping("/")
-    public String testmain(){
-        return "this is ok";
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
     }
 }
